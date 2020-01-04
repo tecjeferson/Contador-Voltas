fc5dbba858221ffd89d7003a19b98096c4ef30ef
Jeferson.Rocha <tecjeferson@gmail.com>
2020-01-04 19:58:45 -0300

    contador de voltas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './styles.css'
+import MostraVoltas from './MostraVoltas'
+import MostraTempo from './MostraTempo'
+import Button from './Button'
+//useState e o mesmo que Hook
+
 
 function App() {
+  const [numVoltas, setNumVoltas] = useState(0)
+  const [running, setRunning] = useState(false)
+  const [tempo, setTempo] = useState(0)
+
+  useEffect(() => {
+    let timer = null
+
+    if (running) {
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+        //console.log('chamou!')
+      }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+
+      }
+    }
+  }, [running])
+
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
+
+  const decrement = () => {
+    if (numVoltas > 0) {
+      setNumVoltas(numVoltas - 1)
+
+    }
+  }
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+
+  const reset = () => {
+    setNumVoltas(0)
+    setTempo(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <MostraVoltas voltas={numVoltas} />
+
+
+      <Button text='+' className='bigger' onClick={increment} />
+      <Button text='-' className='bigger' onClick={decrement} />
+      {
+        numVoltas > 0 &&
+        <MostraTempo tempo={Math.round(tempo / numVoltas)} />
+      }
+
+      <Button onClick={toggleRunning} text={running ? 'Pausar' : 'Iniciar'} />
+      <Button onClick={reset} text='Reiniciar' />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/MostraVoltas.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const MostraVoltas = (props) => {
+    return (
+        <p className='voltas'>
+            <span>{props.voltas}</span> <br />
+            voltas
+    </p>
+    )
+}
+
+export default MostraVoltas
